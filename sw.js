a8de0c6f36f4e7e9288599c5336c321b76de1eb8
wmadson29-creator <wmadson29@gmail.com>
2026-08-26 14:12:37 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-const CACHE="hybrid-training-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE="hybrid-training-v5";
+const STATIC=["./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+
 self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting()));
 });
 self.addEventListener("activate",event=>{
   event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET")return;
-  event.respondWith(
-    caches.match(event.request).then(hit=>hit||fetch(event.request).then(resp=>{
-      const copy=resp.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return resp;
-    }).catch(()=>caches.match("./index.html")))
-  );
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request,{cache:"no-store"}).then(response=>{
+      const copy=response.clone();caches.open(CACHE).then(cache=>cache.put("./index.html",copy));return response;
+    }).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;
+  }).catch(()=>caches.match(event.request)));
 });
+self.addEventListener("message",event=>{if(event.data==="SKIP_WAITING")self.skipWaiting()});
