ab98992f2b89edcbb33412e2f267c8ad3cb66804
wmadson29-creator <wmadson29@gmail.com>
2026-08-26 18:47:41 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="hybrid-training-v10-3";
+const CACHE="hybrid-training-v12";
 const FALLBACK="./index.html";
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.add(FALLBACK)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
