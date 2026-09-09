b52e45ee75cffe0b4baffaa524af70086ad9b241
wmadson29-creator <wmadson29@gmail.com>
2026-09-08 22:28:37 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const BUILD="36.74";
-const CACHE="hybrid-training-v36-74";
-const FALLBACK="./index.html?v=36.74";
+const BUILD="36.75";
+const CACHE="hybrid-training-v36-75";
+const FALLBACK="./index.html?v=36.75";
 const PRECACHE=[
   FALLBACK,
-  "./manifest-v36.webmanifest?v=36.74",
+  "./manifest-v36.webmanifest?v=36.75",
   "./version.json",
   "./hybrid-training-v34-64.png",
   "./hybrid-training-v34-180.png",
